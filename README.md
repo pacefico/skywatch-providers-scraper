a42b2fe19a3ef7947b2b8e8edf1246b5cb654a10
Paulo Cruz <saopcruz@fit-tecnologia.org>
2017-11-23 15:19:18 -0200

    adding code for AWS function lambda

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# skywatch-providers-scraper
-A simple scraper to retrieve data set provider from skywatch website
+skywatch provider scraper
+===================
+
+This scraper was built to retrieve a list of data set provider from  [skywatch website](https://www.skywatch.co/datasets)
+
+> SkyWatch is on a mission to make satellite data accessible to the
+> world. Every day, trillions of pixels are captured by satellites
+> orbiting our planet. With new applications for this data being
+> discovered every week, the demand for this imagery is increasing
+> across many industries.
+
+Live execution available at AWS Lambda API:
+https://jwqnrdh5v1.execute-api.us-east-1.amazonaws.com/dev/skywatchDataSet
+
+Using a request tool like Postman, execute link above with a POST request.
+
+Should return a json:
+
+    {
+	    "datasets": {
+	        "Name1": "Provider1",
+   	        "Name2": "Provider2",
+    },
+	    "total": 2
+	}
+
--- scrap.py
+++ scrap.py
@@ -1,37 +1,53 @@
-
+from json import dumps
 import requests
 from bs4 import BeautifulSoup
 
-url_page = 'https://www.skywatch.co/datasets'
-page = requests.get(url_page)
-soup = BeautifulSoup(page.text, 'html.parser')
-content = soup.find('div', attrs={'class': 'main-content'} )
 
-my_data_set = {}
-for div in content.find_all('div'):
-    if 'id' in div.attrs:
-        data_block = div.find('div', attrs={'data-block-type': '44'})
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else dumps(res),
+        "headers": {
+            "X-Requested-With": '*',
+            "Access-Control-Allow-Headers": 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with',
+            "Access-Control-Allow-Origin": '*',
+            "Access-Control-Allow-Methods": 'POST,GET,OPTIONS'
+        },
+    }
 
-        if data_block:
-            data_set_name = data_block.find('h2').text
 
-            if data_set_name and data_set_name not in my_data_set:
-                print(data_set_name)
-                if data_set_name and len(data_set_name) > 0:
-                    data_set_provider = ""
-                    data_set_info = div.find('div', attrs={'data-block-type': '2'})
-                    for item in data_set_info.find_all('p'):
-                        if 'provider' in item.text.lower():
-                            data_set_provider = item.text
-                            break
-                    my_data_set[data_set_name] = data_set_provider
+def handler_lambda(event, context):
+    url_page = 'https://www.skywatch.co/datasets'
+    page = requests.get(url_page)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    content = soup.find('div', attrs={'class': 'main-content'} )
 
-response = {
-    "datasets": my_data_set,
-    "total": len(my_data_set)
-}
+    my_data_set = {}
+    for div in content.find_all('div'):
+        if 'id' in div.attrs:
+            data_block = div.find('div', attrs={'data-block-type': '44'})
 
-print(response)
+            if data_block:
+                data_set_name = data_block.find('h2').text
+
+                if data_set_name and data_set_name not in my_data_set:
+                    if data_set_name and len(data_set_name) > 0:
+                        data_set_provider = ""
+                        data_set_info = div.find('div', attrs={'data-block-type': '2'})
+                        for item in data_set_info.find_all('p'):
+                            if 'provider' in item.text.lower():
+                                data_set_provider = item.text
+                                break
+                        my_data_set[data_set_name] = data_set_provider
+
+    response = {
+        "datasets": my_data_set,
+        "total": len(my_data_set)
+    }
+
+    return respond(None, response)
+
+
 
 
 
